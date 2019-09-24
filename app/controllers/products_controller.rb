0f125623a399ce18a684f8d95f9dec44e8cef491
ValdemarRuby <wings6928@gmail.com>
2019-09-24 20:17:42 +0300

    added two products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+    before_action :product, only: %i[show edit update]
+
     def new
       @product = Product.new
     end
@@ -17,7 +19,31 @@ class ProductsController < ApplicationController
       @products = Product.all
     end
 
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      if @product.update(product_params)
+        redirect_to @product
+      else
+        render action: 'edit'
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.delete
+      redirect_to products_path
+    end
+
     private
+
+    def product
+      @product = Product.find(params[:id])
+    end
 
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
