a3b450deb8c3c1c8be5da2166d2986e24429e9ab
ValdemarRuby <wings6928@gmail.com>
2019-09-24 17:01:02 +0300

    added views products and record in db

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,25 @@
 class ProductsController < ApplicationController
-  
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.new(product_params)
+      if @product.valid?
+        @product.save
+        redirect_to products_path
+      else
+        render action: 'new'
+      end
+    end
+
+    def index
+      @products = Product.all
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:title, :description, :image_url, :price)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :price, presence: true
 end
