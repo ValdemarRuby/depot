8f29d8d6fe75d8d0e26b1f3d03b50bb2acd84c37
ValdemarRuby <wings6928@gmail.com>
2019-10-09 14:24:30 +0300

    added model test, block test and added stand for test in db

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
-  validates :description, presence: true
-  validates :image_url, presence: true
-  validates :price, presence: true
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: "URL must point to a jpg, png or gif image!"
+  }
 end
