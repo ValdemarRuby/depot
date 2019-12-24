b3d907fe6c2c3607cd5726f382b417d76db21b49
ValdemarRuby <ValdemarRuby@gmail.com>
2019-12-24 14:10:10 +0200

    update order model, fix notice after create order

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -28,7 +28,6 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
 
     respond_to do |format|
 
@@ -75,6 +74,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id,)
+      params.require(:line_item).permit(:product_id, :cart_id)
     end
 end
