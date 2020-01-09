3feea29054088f494b6a8f3b962f7f89b3637c4d
ValdemarRuby <ValdemarRuby@gmail.com>
2020-01-09 15:24:09 +0200

    change add_html

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,7 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    if params[:set_locale]
+    if params[:locale]
       redirect_to store_url(locale: params[:set_locale])
     else
       @products = Product.order(:title)
