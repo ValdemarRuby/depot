267cb0077e68d7d4c0172bfbea77ce0bcd24b56b
ValdemarRuby <ValdemarRuby@gmail.com>
2020-01-09 22:41:47 +0200

    fix destroy line_item for logout users and change routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,15 @@ Rails.application.routes.draw do
   end
 
   get 'admin/index'
-  #get 'sessions/new'
-  #get 'sessions/create'
-  #get 'sessions/destroy'
   resources :users
   resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
+  #get 'store/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products do
-    get :who_bought, on: :member
-  end
+  #resources :products do
+  #  get :who_bought, on: :member
+  #end
 
   resources :contacts
   scope '(:locale)' do
