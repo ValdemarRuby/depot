2cde1fd23a783238f1c2fa1f826e9ab140468a3f
ValdemarRuby <ValdemarRuby@gmail.com>
2020-01-09 22:12:37 +0200

    add products in (:local) routes

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Depot
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.available_locales = [:en, :es]
-    config.i18n.default_locale = :en
+    config.time_zone = 'Kyiv'
   end
 end
